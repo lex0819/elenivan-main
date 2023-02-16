6b6a500499585203cdb1ed44ccc713564d3e3b30
lex0819 <lexmolnar@gmail.com>
2023-02-16 16:07:48 +0300

    init

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+echo 'Hi gays!
+<br>
+<br>
+
+Welcome to my test environment :)
+<br>
+<br>
+
+<ul>
+<li>
+<a href="https://currency.elenivan.ru/" target="_blank">test react and CB RF api currency exchange</a>
+</li>
+<li>
+<a href="https://search-mus.elenivan.ru/" target="_blank">test vue api searching</a>
+</li>
+<li>
+<a href="https://puls.elenivan.ru/" target="_blank">test landing with PHPMailer</a>
+</li>
+<li>
+<a href="https://zmeika.elenivan.ru/" target="_blank">test easy game</a>
+</li>
+<li>
+<a href="https://petsmall.elenivan.ru/" target="_blank">test vue list products and cart</a>
+</li>
+<li>
+<a href="https://zerkovnoty.ru/" target="_blank">church library on WP. It is production.</a>
+</li>
+<li>
+<a href="https://paintjs.elenivan.ru/" target="_blank">test canvas API drowing with mouse in browser.</a>
+</li>
+<li>
+<a href="https://todos.elenivan.ru/" target="_blank">React todos mini App.</a>
+</li>
+<li>
+<a href="https://images.elenivan.ru/" target="_blank">test php scaning files function about some images.</a>
+</li>
+</ul>';
